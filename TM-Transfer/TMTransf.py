c3c9561c1dc59ce3afe4b71651cc266c399e1b73
rafael.gioffi <rafael.gioffi@atos.net>
2024-03-31 17:30:02 -0300

    new version. in testing

---
+++ TM-Transfer/TMTransf.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL do site
+url = "https://trophymanager.com/transfer/#/de/for/amax/null/cost/aff/"
+
+# Fazendo a requisição GET para obter o conteúdo da página
+response = requests.get(url)
+
+# Verifica se a requisição foi bem-sucedida
+if response.status_code == 200:
+    # Parse do conteúdo HTML
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Encontrando todos os divs com a classe 'player_name'
+    player_name_divs = soup.find_all('div', class_='player_name')
+
+    # Lista para armazenar os links
+    links = []
+
+    # Iterando sobre os divs encontrados
+    for div in player_name_divs:
+        # Encontrando o link dentro do div
+        link = div.find('a')['href']
+        # Adicionando o link à lista
+        links.append(link)
+
+    # Imprimindo os links encontrados
+    for link in links:
+        print(link)
+else:
+    print("Falha ao obter a página:", response.status_code)
